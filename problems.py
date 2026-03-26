ee65f707510d9f6704c10b2c044b01cf630e8aa7
Lamgera <mary.myskina@gmail.com>
2026-03-26 11:47:04 +0300

    Средняя 5.4

--- problems.py
+++ problems.py
@@ -1,36 +1,42 @@
 # Блок 2
 
-# 5.2. (уровень сложности: средний)
+# 5.4. (уровень сложности: средний)
 
-# Задача состоит в том, чтобы корректно декодировать и откорректировать следующее текстовое сообщение. 
-# Известно, что каждое значение в исходной последовательности является 8-битным.
+# Реализованная функция вычисления расстояния Левенштейна дает только миниальное число операций.
+# Было бы полезно узнать, какие именно операции произведены над исходной строкой.
+# Реализуйте функцию lev_dist_ops.
 
-def decode_byte(encoded_int):
-    
-    bin_str = bin(encoded_int)[2:]
-    bin_str = bin_str.zfill(24)
-    decoded_bits = []
-    
-    for i in range(0, 24, 3):
-        triplet = bin_str[i:i+3]
-        if triplet.count('1') >= 2:
-            decoded_bits.append('1')
-        else:
-            decoded_bits.append('0')
-    
-    return int("".join(decoded_bits), 2)
+def lev_dist_ops(s1, s2):
+    n, m = len(s1), len(s2)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    for i in range(n + 1): dp[i][0] = i
+    for j in range(m + 1): dp[0][j] = j
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            cost = 0 if s1[i-1] == s2[j-1] else 1
+            dp[i][j] = min(dp[i-1][j] + 1,
+                           dp[i][j-1] + 1,
+                           dp[i-1][j-1] + cost)
+    ops = []
+    curr_i, curr_j = n, m
+    while curr_i > 0 or curr_j > 0:
+        dist = dp[curr_i][curr_j]
+        if curr_i > 0 and curr_j > 0 and s1[curr_i-1] == s2[curr_j-1]:
+            curr_i -= 1
+            curr_j -= 1
+            continue
+            
+        if curr_i > 0 and curr_j > 0 and dist == dp[curr_i-1][curr_j-1] + 1:
+            ops.append('замена')
+            curr_i -= 1
+            curr_j -= 1
+        elif curr_i > 0 and dist == dp[curr_i-1][curr_j] + 1:
+            ops.append('удаление')
+            curr_i -= 1
+        elif curr_j > 0 and dist == dp[curr_j-1][curr_j] + 1:
+            ops.append('вставка')
+            curr_j -= 1
+            
+    return ops[::-1]
 
-encoded_data = [
-    815608, 2064837, 2093080, 2063879, 196608, 2067983, 
-    10457031, 1830912, 2067455, 2093116, 1044928, 2064407, 
-    6262776, 2027968, 4423680, 2068231, 2068474, 1999352, 
-    1019903, 2093113, 2068439, 2064455, 1831360, 1936903, 
-    2067967, 2068456
-]
-
-decoded_bytes = [decode_byte(val) for val in encoded_data]
-
-message = bytes(decoded_bytes).decode('utf-8')
-
-print(f"БАЙТЫ:\n {decoded_bytes}")
-print(f"СООБЩЕНИЕ:\n {message}")
+print(lev_dist_ops('столб', 'слон'))
